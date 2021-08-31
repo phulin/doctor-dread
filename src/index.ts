21bdb09050dcaa02bd89cbce7c4641e979b82609
Aaron McGuire <aaron@gothicginobili.com>
2021-08-31 11:21:40 -0400

    apparently the parasol pref is worse than the navel pref lol

--- src/index.ts
+++ src/index.ts
@@ -103,7 +103,7 @@ function macroPreRun() {
         )
         .trySkill($skill`Use the Force`)
         .tryHaveItem($item`glob of Blank-Out`)
-        .externalIf(get("parasolUsed") < 10, Macro.tryHaveItem($item`peppermint parasol`))
+        .externalIf(get("_navelRunaways") < 10, Macro.tryHaveItem($item`peppermint parasol`))
         .tryHaveItem($item`Louder Than Bomb`)
         .tryHaveItem($item`divine champagne popper`)
     )
