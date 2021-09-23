8f800e337d0cac371ef0ba61d078b4cdfd17c4e6
Patrick Hulin <patrick.hulin@gmail.com>
2021-09-23 07:56:16 -0400

    fix status

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,17 +1,40 @@
 import { print } from "kolmafia";
 
 import { Command } from "./command";
-import { dreadBanished } from "../dungeon/raidlog";
+import {
+  dreadBanished,
+  DreadElement,
+  DreadMonster,
+  DreadZone,
+  isDreadElement,
+  isDreadMonster,
+} from "../dungeon/raidlog";
 
 export const statusCommand = new Command(
   "status",
   "dr status: Print current banishing status of dungeon.",
   () => {
     const banished = dreadBanished();
-    for (const zone of banished) {
-      print(`${zone.zone.toUpperCase()}:`);
-      print(`Elements banished: ${zone.elementsBanished.join(", ")}`);
-      print(`Monsters banished: ${zone.monstersBanished.join(", ")}`);
+    const zoneMap = new Map<DreadZone, [DreadElement[], DreadMonster[]]>();
+    for (const banish of banished) {
+      let zoneInfo = zoneMap.get(banish.targetZone);
+      if (zoneInfo === undefined) {
+        zoneInfo = [[], []];
+        zoneMap.set(banish.targetZone, zoneInfo);
+      }
+
+      const banishedThing = banish.banished;
+      if (isDreadElement(banishedThing)) {
+        zoneInfo[0].push(banishedThing);
+      } else if (isDreadMonster(banishedThing)) {
+        zoneInfo[1].push(banishedThing);
+      }
+    }
+
+    for (const [zone, [elementsBanished, monstersBanished]] of zoneMap) {
+      print(`${zone.toUpperCase()}:`);
+      print(`Elements banished: ${elementsBanished.join(", ")}`);
+      print(`Monsters banished: ${monstersBanished.join(", ")}`);
       print();
     }
   }
