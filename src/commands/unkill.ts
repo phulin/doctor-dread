f5cb001f8d490817fe971321950681bbcb806082
Patrick Hulin <patrick.hulin@gmail.com>
2021-09-25 15:23:32 -0400

    fix typo!

--- src/commands/unkill.ts
+++ src/commands/unkill.ts
@@ -1,34 +1,36 @@
 import {
   cliExecute,
   eat,
-  equip,
+  familiarWeight,
   fullnessLimit,
   myFullness,
+  myMaxhp,
   myPath,
+  numericModifier,
+  print,
   retrieveItem,
   useFamiliar,
-  useSkill,
 } from "kolmafia";
 import {
   $effect,
+  $effects,
   $familiar,
   $item,
   $items,
   $location,
   $skill,
-  $skills,
   have,
   maximizeCached,
   Mood,
 } from "libram";
 import { adventureMacro, Macro } from "../combat";
-import { dreadKilled } from "../dungeon/raidlog";
+import { dreadBanished, dreadKilled } from "../dungeon/raidlog";
 import { propertyManager } from "../lib";
 
 import { Command } from "./command";
 
-const requiredSkills = $skills`Song of Sauce, Carol of the Hells, Wizard Squint, Sauce Monocle, Frigidalmatian`;
-const requiredItems = $items`Novelty Belt Buckle of Violence, unwrapped knock-off retro superhero cape, Unkillable Skeleton's shield`;
+const requiredSkills: Skill[] = [];
+const requiredItems = $items`Unkillable Skeleton's shield`;
 
 export const unkillCommand = new Command(
   "unkill",
@@ -40,6 +42,13 @@ export const unkillCommand = new Command(
     ];
     if (castleKilled < 1000) {
       throw "You haven't finished the castle.";
+    }
+
+    const banished = dreadBanished();
+    const vampiresBanished = banished.filter(({ banished }) => banished === "vampire").length;
+    const skeletonsBanished = banished.filter(({ banished }) => banished === "skeleton").length;
+    if (skeletonsBanished < 2 || vampiresBanished === 2) {
+      throw "Can't guarantee UKS - tweak banishes or do this manually.";
     }
 
     if (myPath() === "Two Crazy Random Summer") {
@@ -69,47 +78,60 @@ export const unkillCommand = new Command(
 
     const mood = new Mood();
 
-    mood.skill($skill`Frigidalmatian`);
+    if (have($skill`Frigidalmatian`)) mood.skill($skill`Frigidalmatian`);
 
     mood.potion($item`corrupted marrow`, 2000);
-    mood.potion($item`tobiko marble soda`, 2000);
-    mood.potion($item`Mer-kin smartjuice`, 5000);
-    mood.potion($item`Hawking's Elixir of Brilliance`, 5000);
-    mood.potion($item`ointment of the occult`, 5000);
-    mood.potion($item`potion of temporary gr8ness`, 5000);
+    mood.potion($item`short white`, 5000);
 
-    mood.skill($skill`Song of Sauce`);
-    mood.skill($skill`Carol of the Hells`);
+    // if (have($item`Powerful Glove`) && !have($effect`Triple-Sized`)) {
+    //   equip($item`Powerful Glove`);
+    //   useSkill($skill`CHEAT CODE: Triple Size`);
+    // }
 
-    // Base 9% spell critical
-    mood.skill($skill`Wizard Squint`); // 19%
-    mood.skill($skill`Sauce Monocle`); // 24%
-    mood.potion($item`natto marble soda`, 1000); // 39%
-    mood.potion($item`invisible potion`, 1000); // 54%
-    mood.potion($item`LOV Elixir #6`, 1000); // 69% - nice!
+    for (const effect of $effects`Triple-Sized, Big`) cliExecute(`shrug ${effect}`);
 
-    // -ML
-    mood.potion($item`cuppa Monstrosi tea`, 1000);
+    useFamiliar($familiar`Mosquito`);
+
+    const forceEquip = $items`Unkillable Skeleton's shield, Mer-kin gutgirdle`;
+    if (have($skill`Frigidalmatian`)) {
+      forceEquip.push(
+        ...$items`meteorb, unwrapped knock-off retro superhero cape, dark baconstone ring`
+      );
+      // mood.potion($item`tobiko marble soda`, 2000);
+      // mood.potion($item`Mer-kin smartjuice`, 5000);
+      // mood.potion($item`Hawking's Elixir of Brilliance`, 5000);
+      // mood.potion($item`ointment of the occult`, 5000);
+      // mood.potion($item`potion of temporary gr8ness`, 5000);
+
+      // mood.skill($skill`Song of Sauce`);
+      // mood.skill($skill`Carol of the Hells`);
+
+      // Base 9% spell critical
+      // mood.skill($skill`Wizard Squint`); // 19%
+      // mood.skill($skill`Sauce Monocle`); // 24%
+      // mood.potion($item`natto marble soda`, 1000); // 39%
+      // mood.potion($item`invisible potion`, 1000); // 54%
+      // mood.potion($item`LOV Elixir #6`, 1000); // 69% - nice!
+
+      // -ML
+      // mood.potion($item`cuppa Monstrosi tea`, 1000);
+    }
+
+    for (const item of forceEquip) retrieveItem(item);
+
+    maximizeCached(["Familiar Weight", "-0.1 HP"], {
+      forceEquip,
+    });
 
     if (!mood.execute()) {
       throw "Failed to get all necessary effects for some reason.";
     }
 
-    retrieveItem($item`meteorb`);
-    retrieveItem($item`dark baconstone ring`);
-
-    if (have($item`Powerful Glove`) && !have($effect`Triple-Sized`)) {
-      equip($item`Powerful Glove`);
-      useSkill($skill`CHEAT CODE: Triple Size`);
-    }
-
-    useFamiliar($familiar`Left-Hand Man`);
-
-    maximizeCached(["Mysticality", "10 Spell Damage Percent", "-100 Monster Level", "-HP"], {
-      forceEquip: $items`Novelty Belt Buckle of Violence, meteorb, unwrapped knock-off retro superhero cape, Unkillable Skeleton's shield, dark baconstone ring`,
-    });
-
     cliExecute("retrocape heck kill");
+
+    if (myMaxhp() > familiarWeight($familiar`Mosquito`) + numericModifier("Familiar Weight")) {
+      print("Your max HP is too high! Please reduce it below 100.");
+    }
 
     propertyManager.setChoices({ 760: 1 });
 
