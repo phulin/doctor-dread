49a7c43ae59d6da23dd942dcb54f658b940290c9
Patrick Hulin <patrick.hulin@gmail.com>
2021-09-29 21:24:20 -0400

    Rename to dr all.

--- src/commands/all.ts
+++ src/commands/all.ts
@@ -84,81 +84,85 @@ function planAllNoncombats(available: Map<Item, number>): [string, NoncombatPlan
   return plan;
 }
 
-export default new Command("daily", "dr daily: Collect useful items from all instances.", () => {
-  printHtml("<b>Doctor Dread: Dailies</b>");
+export default new Command(
+  "all",
+  "dr all: Collect useful items from all instances (and grind flour).",
+  () => {
+    printHtml("<b>Doctor Dread: Dailies</b>");
 
-  if (clans().length === 0) {
-    print("Need to set property dr_clans to a |-separated list of clan names first.", "red");
-    print("Dr. Dread will check the first clan for stash items.");
-    print(
-      "For example: set dr_clans = Flour Garden 1|Flour Garden 2|Flour Garden 3|Flour Garden 4|Flour Garden 5"
-    );
-    return;
-  }
-
-  if (!have($item`Dreadsylvanian skeleton key`) && itemAmount($item`Freddy Kruegerand`) < 100) {
-    throw "You don't have skeleton keys and you're almost out of Freddies. Fix that.";
-  }
-
-  print(`Checking clans ${clans().join(", ")}`);
-
-  const originalClan = Clan.get();
-  const acquired = new Map<Item, number>();
-
-  let flourCount = 0;
-
-  try {
-    let plan = planAllNoncombats(new Map());
-
-    flourCount = sum(
-      plan,
-      ([, noncombatPlans]) =>
-        noncombatPlans.filter(([, , , item]) => item === $item`bone flour`).length
-    );
-
-    let boneCount = 0;
-    if (flourCount > 0) {
-      const stashClan = Clan.join(clans()[0]);
-      const taken = stashClan.take(new Map([[$item`old dry bone`, flourCount]]));
-      boneCount = taken.get($item`old dry bone`) ?? 0;
-    }
-
-    if (boneCount < flourCount) {
+    if (clans().length === 0) {
+      print("Need to set property dr_clans to a |-separated list of clan names first.", "red");
+      print("Dr. Dread will check the first clan for stash items.");
       print(
-        `Failed to take ${flourCount} old dry bones from stash! Replanning with ${boneCount}...`
+        "For example: set dr_clans = Flour Garden 1|Flour Garden 2|Flour Garden 3|Flour Garden 4|Flour Garden 5"
       );
-      plan = planAllNoncombats(new Map([[$item`old dry bone`, boneCount]]));
+      return;
     }
 
-    withWineglass(() => {
-      for (const [clanName, noncombatPlans] of plan) {
-        Clan.join(clanName);
+    if (!have($item`Dreadsylvanian skeleton key`) && itemAmount($item`Freddy Kruegerand`) < 100) {
+      throw "You don't have skeleton keys and you're almost out of Freddies. Fix that.";
+    }
 
-        for (const [noncombat, subIndex, choiceIndex, item] of noncombatPlans) {
-          print(`Getting ${item}.`);
-          const subnoncombat = noncombat.choices.get(subIndex);
-          propertyManager.setChoices({
-            [noncombat.id]: subIndex,
-            [subnoncombat?.id ?? -1]: choiceIndex,
-          });
+    print(`Checking clans ${clans().join(", ")}`);
 
-          if (subnoncombat?.isLocked()) retrieveItem($item`Dreadsylvanian skeleton key`);
-          visitUrl(`clan_dreadsylvania.php?action=forceloc&loc=${noncombat.index}`);
-          runChoice(-1);
-          if (handlingChoice()) throw "Stuck in choice adventure!";
-          acquired.set(item, (acquired.get(item) ?? 0) + 1);
-          print();
-        }
+    const originalClan = Clan.get();
+    const acquired = new Map<Item, number>();
+
+    let flourCount = 0;
+
+    try {
+      let plan = planAllNoncombats(new Map());
+
+      flourCount = sum(
+        plan,
+        ([, noncombatPlans]) =>
+          noncombatPlans.filter(([, , , item]) => item === $item`bone flour`).length
+      );
+
+      let boneCount = 0;
+      if (flourCount > 0) {
+        const stashClan = Clan.join(clans()[0]);
+        const taken = stashClan.take(new Map([[$item`old dry bone`, flourCount]]));
+        boneCount = taken.get($item`old dry bone`) ?? 0;
       }
-    });
-  } finally {
-    if (acquired.size > 0) {
-      const stashClan = Clan.join(clans()[0]);
-      stashClan.put(acquired);
+
+      if (boneCount < flourCount) {
+        print(
+          `Failed to take ${flourCount} old dry bones from stash! Replanning with ${boneCount}...`
+        );
+        plan = planAllNoncombats(new Map([[$item`old dry bone`, boneCount]]));
+      }
+
+      withWineglass(() => {
+        for (const [clanName, noncombatPlans] of plan) {
+          Clan.join(clanName);
+
+          for (const [noncombat, subIndex, choiceIndex, item] of noncombatPlans) {
+            print(`Getting ${item}.`);
+            const subnoncombat = noncombat.choices.get(subIndex);
+            propertyManager.setChoices({
+              [noncombat.id]: subIndex,
+              [subnoncombat?.id ?? -1]: choiceIndex,
+            });
+
+            if (subnoncombat?.isLocked()) retrieveItem($item`Dreadsylvanian skeleton key`);
+            visitUrl(`clan_dreadsylvania.php?action=forceloc&loc=${noncombat.index}`);
+            runChoice(-1);
+            if (handlingChoice()) throw "Stuck in choice adventure!";
+            acquired.set(item, (acquired.get(item) ?? 0) + 1);
+            print();
+          }
+        }
+      });
+    } finally {
+      if (acquired.size > 0) {
+        const stashClan = Clan.join(clans()[0]);
+        stashClan.put(acquired);
+      }
+      if (acquired.get($item`bone flour`) ?? 0 > 0) {
+        set("_dr_groundFlour", true);
+      }
+      originalClan.join();
     }
-    if (acquired.get($item`bone flour`) ?? 0 > 0) {
-      set("_dr_groundFlour", true);
-    }
-    originalClan.join();
   }
-});
+);
