f3fb9e1fee3c9410509faa7a48caa2ead2101ac4
Patrick Hulin <patrick.hulin@gmail.com>
2021-09-29 21:21:04 -0400

    Add daily collection command.

--- src/commands/daily.ts
+++ src/commands/daily.ts
@@ -9,7 +9,7 @@ import {
   runChoice,
   visitUrl,
 } from "kolmafia";
-import { $item, $items, $stat, Clan, get, have, sum } from "libram";
+import { $item, $items, $stat, Clan, get, have, set, sum } from "libram";
 
 import { Command } from "../command";
 import { DreadNoncombat, dreadNoncombatsUsed, dreadZones } from "../dungeon/raidlog";
@@ -17,7 +17,7 @@ import { clans, entries, propertyManager, withWineglass } from "../lib";
 
 type NoncombatPlan = [DreadNoncombat, number, number, Item];
 
-export default new Command("daily", "dr daily: Collect useful items from all instances.", () => {
+function planAllNoncombats(available: Map<Item, number>): [string, NoncombatPlan[]][] {
   const grindFlour = !get("_dr_groundFlour", false) && myPrimestat() === $stat`Muscle`;
 
   const items = $items`dreadful roast, stinking agaricus, dread tarragon, wax banana, eau de mort`;
@@ -27,6 +27,64 @@ export default new Command("daily", "dr daily: Collect useful items from all ins
     entries(items).map(([index, item]) => [item, index]) as [Item, number][]
   );
 
+  const plan: [string, NoncombatPlan[]][] = [];
+
+  for (const clanName of clans()) {
+    Clan.join(clanName);
+
+    // noncombat, sub, choice, item
+    const result: [DreadNoncombat, number, number, Item][] = [];
+
+    const used = dreadNoncombatsUsed();
+    for (const zone of dreadZones) {
+      for (const noncombat of zone.noncombats) {
+        if (used.includes(noncombat.name)) continue;
+
+        let currentPriority = 999;
+        let currentResult: NoncombatPlan | undefined = undefined;
+
+        for (const [subIndex, subnoncombat] of noncombat.choices) {
+          if (subnoncombat.classes && !subnoncombat.classes.includes(myClass())) continue;
+
+          for (const [choiceIndex, choice] of subnoncombat.choices) {
+            if (choice.maximum && choice.count() >= choice.maximum) continue;
+            if (!choice.item) continue;
+
+            if (choice.requirement) {
+              const requirementsAvailable =
+                available.get(choice.requirement) ?? itemAmount(choice.requirement);
+              if (requirementsAvailable <= 0) continue;
+            }
+
+            const priority = itemPriority.get(choice.item) ?? 999;
+            if (priority < currentPriority) {
+              currentPriority = priority;
+              currentResult = [noncombat, subIndex, choiceIndex, choice.item];
+            }
+          }
+        }
+
+        if (currentResult) {
+          result.push(currentResult);
+          const [noncombat, subIndex, choiceIndex] = currentResult;
+          const choice = noncombat.choices.get(subIndex)?.choices.get(choiceIndex);
+          if (choice?.requirement) {
+            available.set(
+              choice.requirement,
+              (available.get(choice.requirement) ?? itemAmount(choice.requirement)) - 1
+            );
+          }
+        }
+      }
+    }
+
+    plan.push([clanName, result]);
+  }
+
+  return plan;
+}
+
+export default new Command("daily", "dr daily: Collect useful items from all instances.", () => {
   printHtml("<b>Doctor Dread: Dailies</b>");
 
   if (clans().length === 0) {
@@ -44,59 +102,32 @@ export default new Command("daily", "dr daily: Collect useful items from all ins
 
   print(`Checking clans ${clans().join(", ")}`);
 
-  const plan: [string, NoncombatPlan[]][] = [];
-
   const originalClan = Clan.get();
+  const acquired = new Map<Item, number>();
+
+  let flourCount = 0;
 
   try {
-    for (const clanName of clans()) {
-      Clan.join(clanName);
+    let plan = planAllNoncombats(new Map());
 
-      // noncombat, sub, choice, item
-      const result: [DreadNoncombat, number, number, Item][] = [];
-
-      const used = dreadNoncombatsUsed();
-      for (const zone of dreadZones) {
-        for (const noncombat of zone.noncombats) {
-          if (used.includes(noncombat.name)) continue;
-
-          let currentPriority = 999;
-          let currentResult: NoncombatPlan | undefined = undefined;
-
-          for (const [subIndex, subnoncombat] of noncombat.choices) {
-            if (subnoncombat.classes && !subnoncombat.classes.includes(myClass())) continue;
-
-            for (const [choiceIndex, choice] of subnoncombat.choices) {
-              if (choice.maximum && choice.count() >= choice.maximum) continue;
-              if (!choice.item) continue;
-
-              const priority = itemPriority.get(choice.item) ?? 999;
-              if (priority < currentPriority) {
-                currentPriority = priority;
-                currentResult = [noncombat, subIndex, choiceIndex, choice.item];
-              }
-            }
-          }
-
-          if (currentResult) {
-            result.push(currentResult);
-          }
-        }
-      }
-
-      plan.push([clanName, result]);
-    }
-
-    const flourCount = sum(
+    flourCount = sum(
       plan,
       ([, noncombatPlans]) =>
         noncombatPlans.filter(([, , , item]) => item === $item`bone flour`).length
     );
 
+    let boneCount = 0;
     if (flourCount > 0) {
-      const stashClanName = clans()[0];
-      const stashClan = Clan.join(stashClanName);
-      stashClan.take(new Map([[$item`old dry bone`, flourCount]]));
+      const stashClan = Clan.join(clans()[0]);
+      const taken = stashClan.take(new Map([[$item`old dry bone`, flourCount]]));
+      boneCount = taken.get($item`old dry bone`) ?? 0;
+    }
+
+    if (boneCount < flourCount) {
+      print(
+        `Failed to take ${flourCount} old dry bones from stash! Replanning with ${boneCount}...`
+      );
+      plan = planAllNoncombats(new Map([[$item`old dry bone`, boneCount]]));
     }
 
     withWineglass(() => {
@@ -115,11 +146,19 @@ export default new Command("daily", "dr daily: Collect useful items from all ins
           visitUrl(`clan_dreadsylvania.php?action=forceloc&loc=${noncombat.index}`);
           runChoice(-1);
           if (handlingChoice()) throw "Stuck in choice adventure!";
+          acquired.set(item, (acquired.get(item) ?? 0) + 1);
           print();
         }
       }
     });
   } finally {
+    if (acquired.size > 0) {
+      const stashClan = Clan.join(clans()[0]);
+      stashClan.put(acquired);
+    }
+    if (acquired.get($item`bone flour`) ?? 0 > 0) {
+      set("_dr_groundFlour", true);
+    }
     originalClan.join();
   }
 });
